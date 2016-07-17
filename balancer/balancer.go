9e539154ac43639549fe8e5581f678714c1a9ff8
root <sturdevant2@wisc.edu>
2016-07-17 19:27:51 -0400

    Cleaned things up and allowed for many calls to be made from client

--- balancer/balancer.go
+++ balancer/balancer.go
@@ -7,14 +7,11 @@ import (
 	"io"
 	"log"
 	"net"
-	"sync"
-	"time"
 
 	"github.com/open-lambda/load-balancer/balancer/connPeek"
 	"github.com/open-lambda/load-balancer/balancer/serverPick"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
-	_ "google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/transport"
 
 	pb "google.golang.org/grpc/examples/helloworld/helloworld"
@@ -52,7 +49,7 @@ func runBalancer(address string, chooser serverPick.ServerPicker) {
 	for {
 		conn1, err := lis.Accept()
 		if err != nil {
-			panic("oh no!")
+			panic(err.Error())
 		}
 
 		// Will need access to buf later for proxying
@@ -60,7 +57,7 @@ func runBalancer(address string, chooser serverPick.ServerPicker) {
 		r := io.TeeReader(conn1, &buf)
 
 		// Using conn to peek at the method name w/o affecting buf
-		var conn net.Conn = &connPeek.ReaderConn{Reader: r, Conn: conn1}
+		conn := &connPeek.ReaderConn{Reader: r, Conn: conn1}
 		st, err := transport.NewServerTransport("http2", conn, 100, nil)
 		if err != nil {
 			panic(err.Error())
@@ -76,46 +73,17 @@ func runBalancer(address string, chooser serverPick.ServerPicker) {
 				panic(err.Error())
 			}
 
-			// Call function on every chosen backend
-			pbuf := &buf
-			done := false
-			var cpLock sync.Mutex
-			for i := 0; i < len(servers); i++ {
-				// Will have to split connection across every backend
-				var buf2 bytes.Buffer
-				reader := io.TeeReader(pbuf, &buf2)
-
-				// Submit requests in parallel and choose the winner
-				go func(j int, r2 io.Reader) {
-					fmt.Printf("Server chosen to run on: %v\n", servers[j])
-					// Dial given server number
-					conn2, err := net.Dial("tcp", servers[j])
-					defer conn2.Close()
-					if err != nil {
-						panic(err.Error())
-					}
-
-					// Run function on chosen server
-					io.Copy(conn2, r2)
-					if j == 0 {
-						time.Sleep(time.Minute)
-					}
-
-					// Copy response back to client only if this backend "won"
-					cpLock.Lock()
-					if !done {
-						io.Copy(conn1, conn2)
-						done = true
-					}
-					cpLock.Unlock()
-
-				}(i, reader)
-
-				// Set pbuf to next buffer so we can split connection again
-				pbuf = &buf2
-			}
+			// Actually send the request to "best" backend (first one for now)
+			go func() {
+				conn2, err := net.Dial("tcp", servers[0])
+				if err != nil {
+					panic(err.Error())
+				}
+				conn.SecondConn(conn2) // This is so we can close conn2 cleanly
+				io.Copy(conn2, &buf)
+				io.Copy(conn1, conn2)
+			}()
 		})
-
 	}
 }
 
@@ -130,7 +98,7 @@ func runClient(address string) {
 
 	// Contact the server and print out its response.
 	name := "tyler"
-	r, err := c.SayHello(context.Background(), &pb.HelloRequest{Name: name /*, Num: 5*/})
+	r, err := c.SayHello(context.Background(), &pb.HelloRequest{Name: name})
 	if err != nil {
 		log.Fatalf("could not greet: %v", err)
 	}
@@ -138,11 +106,18 @@ func runClient(address string) {
 }
 
 func main() {
-	servers := []string{"localhost:5052", "localhost:5053", "localhost:5054"}
-	for i := 0; i < len(servers); i++ {
-		go runServer(servers[i])
+	var servers []string
+	for i := 0; i < 3; i++ {
+		server := "localhost:" + fmt.Sprintf("%v", i+5052)
+		go runServer(server)
+		servers = append(servers, server)
 	}
+
 	chooser := serverPick.NewFirstTwo(servers)
+	//chooser := serverPick.NewRandPicker(servers)
 	go runBalancer(lbAddr, chooser)
-	runClient(lbAddr)
+	for i := 0; ; i++ {
+		fmt.Printf("Client's been run %v time(s)\n", i)
+		runClient(lbAddr)
+	}
 }
--- balancer/connPeek/connPeek.go
+++ balancer/connPeek/connPeek.go
@@ -6,11 +6,20 @@ import (
 	"time"
 )
 
+/*
+ * Main purpose of this struct is a hack to enable connection peeking without
+ * disrupting the ability of the loadbalancer to proxy between client & server.
+ */
 type ReaderConn struct {
 	Reader io.Reader
 	Conn   net.Conn
+	conn2  net.Conn
 }
 
+/*
+ * Read and write will operate on the reader so that we can still read from the
+ * connection!
+ */
 func (r *ReaderConn) Read(b []byte) (n int, err error) {
 	return r.Reader.Read(b)
 }
@@ -19,10 +28,26 @@ func (r *ReaderConn) Write(b []byte) (n int, err error) {
 	return len(b), nil
 }
 
+/*
+ * This function is a little strange, but it's a way to setup a clean way to
+ * close the connection on the server end.
+ */
+func (r *ReaderConn) SecondConn(c2 net.Conn) {
+	r.conn2 = c2
+}
+
+/*
+ * This function gets called automatically by grpc's HandleStreams. This way we
+ * also know it's safe to close the connection to the server at the same time.
+ */
 func (r *ReaderConn) Close() error {
+	r.conn2.Close()
 	return r.Conn.Close()
 }
 
+/*
+ * All the rest just call the functions from Conn
+ */
 func (r *ReaderConn) LocalAddr() net.Addr {
 	return r.Conn.LocalAddr()
 }
@@ -32,13 +57,13 @@ func (r *ReaderConn) RemoteAddr() net.Addr {
 }
 
 func (r *ReaderConn) SetDeadline(t time.Time) error {
-	panic("not implemented")
+	return r.Conn.SetDeadline(t)
 }
 
 func (r *ReaderConn) SetReadDeadline(t time.Time) error {
-	panic("not implemented")
+	return r.Conn.SetReadDeadline(t)
 }
 
 func (r *ReaderConn) SetWriteDeadline(t time.Time) error {
-	panic("not implemented")
+	return r.Conn.SetWriteDeadline(t)
 }
