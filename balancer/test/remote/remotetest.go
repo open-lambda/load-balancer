5195477ffc9897510fc32d382a3dda83b0829eba
Ed Oakes <eoakes@wisc.edu>
2016-07-21 17:57:43 -0400

    implemented DropletsFromConfig

--- balancer/test/remote/remotetest.go
+++ balancer/test/remote/remotetest.go
@@ -2,19 +2,31 @@ package main
 
 import (
 	"encoding/json"
-	"os"
+	"fmt"
 	"log"
+	"os"
 
-	"golang.org/x/oauth2"
 	"github.com/digitalocean/godo"
+	"golang.org/x/oauth2"
 )
 
-type ServerConfig struct {
-	Servers []struct {
-		Region string
-		Size string
-		Number int
-	}
+const BASE_IMAGE = "ubuntu-14-04-x64"
+const NUM_CLIENTS = 1
+
+type DropletConfig struct {
+	Region string
+	Size   string
+	Number int
+}
+
+type TestConfig struct {
+	Servers   []DropletConfig
+	Clients   []DropletConfig
+	Balancers []DropletConfig
+}
+
+type LBConfig struct {
+	Servers []string
 }
 
 type TokenSource struct {
@@ -22,26 +34,50 @@ type TokenSource struct {
 }
 
 func (t *TokenSource) Token() (*oauth2.Token, error) {
-	token := &oauth2.Token {
+	token := &oauth2.Token{
 		AccessToken: t.AccessToken,
 	}
 
 	return token, nil
 }
 
-func CreateClients() {
-	return
+func DropletsFromConfig(client *godo.Client, conf []DropletConfig, name string) []godo.Droplet {
+	servers := make([]godo.Droplet, 0)
+	for k := 0; k < len(conf.Servers); k++ {
+		if conf.Servers[k].Number == 1 {
+			request := &godo.DropletCreateRequest{
+				Name:   fmt.Sprintf("%s-%d", name, k),
+				Region: conf.Servers[k].Region,
+				Size:   conf.Servers[k].Size,
+				Image: godo.DropletCreateImage{
+					Slug: BASE_IMAGE,
+				},
+			}
+			servers = append(servers, []godo.Droplet{*CreateDroplet(client, request)}...)
+
+		} else {
+			names := make([]string, conf.Servers[k].Number)
+			for i := 0; i < conf.Servers[k].Number; i++ {
+				names[i] = fmt.Sprintf("%s-%d-%d", name, k, i)
+			}
+
+			request := &godo.DropletMultiCreateRequest{
+				Names:  names,
+				Region: conf.Servers[k].Region,
+				Size:   conf.Servers[k].Size,
+				Image: godo.DropletCreateImage{
+					Slug: BASE_IMAGE,
+				},
+			}
+			servers = append(servers, CreateDroplets(client, request)...)
+		}
+
+	}
+
+	return servers
 }
 
-func CreateLB() {
-	return
-}
-
-func CreateServers() {
-	return
-}
-
-func CreateDroplets(client *godo.Client, request *godo.DropletMultiCreateRequest, num int) []godo.Droplet {
+func CreateDroplets(client *godo.Client, request *godo.DropletMultiCreateRequest) []godo.Droplet {
 	newDroplets, _, err := client.Droplets.CreateMultiple(request)
 
 	if err != nil {
@@ -61,14 +97,14 @@ func CreateDroplet(client *godo.Client, request *godo.DropletCreateRequest) *god
 	return newDroplet
 }
 
-func readServerConfig(filename string) *ServerConfig {
+func ReadTestConfig(filename string) *TestConfig {
 	fd, err := os.Open(filename)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	decoder := json.NewDecoder(fd)
-	conf := ServerConfig{}
+	conf := TestConfig{}
 
 	err = decoder.Decode(&conf)
 	if err != nil {
@@ -79,42 +115,38 @@ func readServerConfig(filename string) *ServerConfig {
 }
 
 func main() {
-	pat := os.Getenv("DO_AUTHENTICATION_TOKEN")	
+	if os.Getenv("DO_PUBLIC_KEY_ID") == "" {
+		log.Fatal("DO_PUBLIC_KEY_ID environment variable not set. Please use ssh_key tool before attempting remote test again")
+	}
+
+	pat := os.Getenv("DO_AUTHENTICATION_TOKEN")
 	if pat == "" {
 		log.Fatal("DO_AUTHENTICATION_TOKEN environment variable not set")
 	}
 
-	tokenSource := &TokenSource {
+	tokenSource := &TokenSource{
 		AccessToken: pat,
 	}
 	oauthClient := oauth2.NewClient(oauth2.NoContext, tokenSource)
 	client := godo.NewClient(oauthClient)
 
-	// TODO add config file for servers
-	// TODO spin up droplets for servers
-	// TODO copy runserver binary for servers
+	conf := ReadServerConfig("test.conf")
+
+	// spin up droplets for servers
+	servers := DropletsFromConfig(client, conf.Servers, "server")
+
+	// TODO scp runserver binary for servers
 	// TODO write config for the load balancer
 
 	// TODO spin up droplet for load balancer
+	balancers := DropletsFromConfig(client, conf.Balancers, "loadbalancer")
 	// TODO write config for the clients
 
-	// TODO spin up droplets for clients
-	// TODO copy runclient binary and client.conf config file
-	// TODO run clients
+	// spin up droplets for clients
+	clients := DropletsFromConfig(client, conf.Clients, "client")
+	// TODO scp runclient binary and client.conf config file
+
+	// TODO run clients (with a timeout)
 
 	// TODO delete droplets
-
-	dropletName := "super-cool-droplet"
-	createRequest := &godo.DropletCreateRequest {
-		Name: dropletName,
-		Region: "nyc3",
-		Size: "512mb",
-		Image: godo.DropletCreateImage {
-			Slug: "ubuntu-14-04-x64",
-		},
-	}
-	
-	//newDroplet, _, err := client.Droplets.Create(createRequest)
-	CreateDroplet(client, createRequest)
-
 }
