cc7673256884bbd2719b062eb0d43e4c81115da0
Ed Oakes <eoakes@wisc.edu>
2016-08-11 15:55:47 -0400

    tidied up

--- balancer/inspect/codegen/codegen.go
+++ balancer/inspect/codegen/codegen.go
@@ -10,21 +10,7 @@ import (
 	"path/filepath"
 )
 
-func Generate(file []byte, name string) ([]byte, error) {
-	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
-	if err != nil {
-		return nil, err
-	}
-
-	protofile := filepath.Join(dir, fmt.Sprintf("%s.proto", name))
-	fmt.Println(protofile)
-	n := bytes.IndexByte(file, 0)
-	err = ioutil.WriteFile(protofile, file[:n], 0644)
-	if err != nil {
-		return nil, err
-	}
-	defer os.Remove(protofile)
-
+func genPB(dir, protofile, name string) ([]byte, error) {
 	go_out := fmt.Sprintf("--go_out=%s", dir)
 	proto_path := fmt.Sprintf("--proto_path=%s", dir)
 
@@ -32,18 +18,45 @@ func Generate(file []byte, name string) ([]byte, error) {
 	var e bytes.Buffer
 	cmd.Stderr = &e
 
-	err = cmd.Run()
+	err := cmd.Run()
 	if err != nil {
 		return nil, errors.New(e.String())
 	}
 
-	defer os.Remove(fmt.Sprintf("%s/%s.pb.go", dir, name))
+	pbfile := fmt.Sprintf("%s/%s.pb.go", dir, name)
+	defer os.Remove(pbfile)
 
-	pbfile := fmt.Sprintf("%s.pb.go", name)
 	pb, err := ioutil.ReadFile(pbfile)
 	if err != nil {
 		return nil, err
 	}
 
 	return pb, nil
+}
+
+func writeProto(file []byte, dir, name string) (string, error) {
+	protofile := filepath.Join(dir, fmt.Sprintf("%s.proto", name))
+	n := bytes.IndexByte(file, 0)
+
+	err := ioutil.WriteFile(protofile, file[:n], 0644)
+	if err != nil {
+		return "", err
+	}
+
+	return protofile, nil
+}
+
+func Generate(file []byte, name string) ([]byte, error) {
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		return nil, err
+	}
+
+	protofile, err := writeProto(file, dir, name)
+	if err != nil {
+		return nil, err
+	}
+	defer os.Remove(protofile)
+
+	return genPB(dir, protofile, name)
 }
